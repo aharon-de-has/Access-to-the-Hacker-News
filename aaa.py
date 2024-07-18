0da3c849e88c83d84846218280fda1ffcde88593
aharon de has <as077300@gmail.com>
2024-07-19 01:49:40 +0300

    comment

--- aaa.py
+++ aaa.py
@@ -13,16 +13,28 @@ def get_top_stories(num):
         story_url = f'{url_main}/item/{story_id}.json'
         story_response = requests.get(story_url)
         story_data = story_response.json()
+        if 'kids' in story_data:
+            comment_ids = story_data['kids']
+            comments = get_cooment(comment_ids)
         stories.append(story_data)
-    return stories
-    
+    return stories, comments
+
+def get_cooment(comment_ids):
+    comments = []
+    for comment_id in comment_ids:
+        comment_url = f'{url_main}/itemm{comment_id}.json'
+        comment_response = requests.get(comment_url)
+        comment_data = comment_response.json()
+        if comment_data and 'text' in comment_data:
+            comments.append(comment_data)
+    return comments
+
+
 def analyze_stories(stories):
     df = pd.DataFrame(stories)
-    df_stories = df[['id', 'title', 'by', 'score','descendants', 'time', 'url', ]]
+    df = df[['id', 'title', 'by', 'score', 'time', 'url']]
     df['time'] = pd.to_datetime(df['time'], unit='s')
-    df_comments = df['kids']
-    return df_stories, df_comments
-
+    return df
 
 # def plot_scores(df):
 #     plt.figure(figsize=(20, 7))
@@ -46,15 +58,16 @@ def save_to_csv(df, filename='top_stories.csv'):
     df.to_csv(filename, index=True)
 
 
-
-top_stories = get_top_stories(10)
-stories_df, comment_df = analyze_stories(top_stories)
+top_stories, comments = get_top_stories(10)
+stories_df = analyze_stories(top_stories)
+comments_df = analyze_stories(comments)
 avrege_score = stories_df['score'].mean()
 stories_df[avrege_score] = avrege_score
 plot_pie_chart(stories_df)
 # plot_scores(stories_df)
 
 save_to_csv(stories_df)
+save_to_csv(comments_df)
 
 
 
